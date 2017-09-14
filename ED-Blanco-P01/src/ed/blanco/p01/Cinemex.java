3d8e51d52c7934c0adf95d7783cdf52317c76a89
Eazuara <A01410412@itesm.mx>
2017-09-13 21:58:38 -0500

    Update Cinemex.java

--- ED-Blanco-P01/src/ed/blanco/p01/Cinemex.java
+++ ED-Blanco-P01/src/ed/blanco/p01/Cinemex.java
@@ -14,37 +14,34 @@ import java.util.Scanner;
  */
 public class Cinemex  {
  
-   ArrayList<Sala> S=new ArrayList<Sala>();
+   protected ArrayList<Sala> S=new ArrayList<Sala>();
    
-     public void registrar(){
+      void registrar(){
         Scanner R = new Scanner(System.in);
-        
-        
-        String pelicula = null;
-        ffor(int j = 0; j<7; j++) S.add(new Sala(j + 1));
+        for(int j = 0; j<7; j++) S.add(new Sala(j + 1));
         for(int i = 0; i<2; i++){
           System.out.println("Ingrese el nombre de la película.");
-            String movie = R.next();
-     }
+            String pelicula = R.next();
+     
         for(int j=0; j<7;j++){
-            System.out.println("Introduzca el nombre de la pelicula");
-         
-            for(int i=0; i<7 ; i++){
             S.get(i).addFuncion(pelicula);//Se introduce la pelicula al array
             int x = S.get(i).Fetch(pelicula);
-            System.out.println("Introduzca el numero de personas que visito esta pelicula en la sala no."+(i+1));
+            System.out.println("Introduzca el numero de personas que visito esta pelicula en la sala no."+(j+1));
             int visitas = R.nextInt();//Depues se pide el numero de visitas
             S.get(i).P.get(x).setVisitas(visitas);
-     }
+            }
         }
-     }
      
-     void combinacion(){//Metodo para sacar la Relacion Pelicula-Visitas
+        }
+       void combinacion(){
          for(Sala sala:S){
-             System.out.println("La pelicula mas vista en la sala" + sala.getNum() + "es" + sala.mostViewed() + "con" + sala.getVisitas() + "Visitas");
+             System.out.println("La pelicula mas vista en la sala " + sala.getNum() + " es " + sala.mostViewed() + " con " + sala.getVisitas() + " Visitas ");
          }
      }
 }
     
+    
+
+  
     
 
